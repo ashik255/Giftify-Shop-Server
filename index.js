22251aa2012aff67821abbdafef200b8a31c62d0
ashik255 <ariful15-11556@diu.edu.bd>
2022-06-17 03:53:03 +0600

    datapush

--- index.js
+++ index.js
@@ -2,22 +2,31 @@ const express = require('express')
 const app = express()
 const cors =require('cors')
 require('dotenv').config()
-const { MongoClient } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
  
 const port = process.env.PORT || 5000
 app.use(cors())
  
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o13hs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o13hs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-// console.log(uri)
+console.log(uri)
  
 async function run(){
 try{
 await client.connect();
-console.log('connected')
+const database = client.db("Giftify-Shop");
+const shopDataCollection = database.collection("shopData");
+
+// shop data
+app.get('/shopData',async(req,res)=>{
+  const curser = shopDataCollection.find({});
+  const data = await curser.toArray();
+  res.send(data);
+})
+
 }
 finally{
     await client.connect();
